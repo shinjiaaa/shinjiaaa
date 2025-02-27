5d71e97faa1dbeda3b6a106e68cd3022ac96b25d
shinjiaaa <134384192+shinjiaaa@users.noreply.github.com>
2025-02-27 14:16:23 +0900

    Title: 사칙연산 연습, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250227/사칙연산 연습/practice-basic-arithmetic-operations.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 30;
+    int c = a + b;
+    cout << c/8 << endl;
+    cout << c%8 << endl;
+    return 0;
+}

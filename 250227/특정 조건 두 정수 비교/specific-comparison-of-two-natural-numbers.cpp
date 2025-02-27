c672fc7d1b98bb3f057596eeb8b9f7dd860604b0
shinjiaaa <134384192+shinjiaaa@users.noreply.github.com>
2025-02-27 15:28:43 +0900

    Title: 특정 조건 두 정수 비교, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250227/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    
+    if(A<B){
+        cout << "1";
+    }
+    else {
+        cout << "0";
+    }
+
+    if(A=B){
+        cout << " 1";
+    }
+    else {
+        cout << " 0"
+    }
+}

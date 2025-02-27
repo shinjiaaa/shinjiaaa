ba17365190dfcb751d7abc704cf7658f22775cba
shinjiaaa <134384192+shinjiaaa@users.noreply.github.com>
2025-02-27 14:56:27 +0900

    Title: 세 실수의 반올림, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250227/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    
+    cout << fixed;
+    cout.precision(3);
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    return 0;
+}

0d96cf6bf8cc5c498e4a24addc4deb348988d6a0
shinjiaaa <134384192+shinjiaaa@users.noreply.github.com>
2025-02-27 15:20:17 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250227/비교 연산/comparison-operator.cpp
+++ 250227/비교 연산/comparison-operator.cpp
@@ -5,41 +5,42 @@ int main() {
     int A, B;
     cin >> A >> B;
 
+    // 1
     if(A>=B){
         cout << "1" << endl;
     }
     else{
         cout << "0" << endl;
     }
-
+    // 2
     if(A>B){
         cout << "1" << endl;
     }
     else{
         cout << "0" << endl;
     }
-
+    // 3
     if(A<=B){
         cout << "1" << endl;
     }
     else{
         cout << "0" << endl;
     }
-
+    // 4
     if(A<B){
         cout << "1" << endl;
     }
     else{
         cout << "0" << endl;
     }
-
-    if(A=B){
+    // 5
+    if(A==B){
         cout << "1" << endl;
     }
     else{
         cout << "0" << endl;
     }
-
+    // 6
     if(A!=B){
         cout << "1" << endl;
     }

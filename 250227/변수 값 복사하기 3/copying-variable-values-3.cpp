53fda425fae41f44d880c2aaef476a559f48357b
shinjiaaa <134384192+shinjiaaa@users.noreply.github.com>
2025-02-27 14:03:19 +0900

    Title: 변수 값 복사하기 3, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250227/변수 값 복사하기 3/copying-variable-values-3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=1;
+    int b=2;
+    int c=3;
+
+    a=c;
+    b=c;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+}

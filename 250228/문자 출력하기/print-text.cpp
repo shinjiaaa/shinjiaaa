c9875141a1532e65799fc136a61c1507cdb21a4a
Shin Jia <134384192+shinjiaaa@users.noreply.github.com>
2025-02-28 12:52:32 +0900

    Title: 문자 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250228/문자 출력하기/print-text.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a;
+    cin >> a;
+
+    for (a = 0; a < 8; a++) {
+        cout << a << endl;
+    }
+    return 0;
+}

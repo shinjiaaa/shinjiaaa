605eaed574cb9ab66ee6aaf238439d26599c80a6
Shin Jia <134384192+shinjiaaa@users.noreply.github.com>
2025-02-28 12:54:28 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250228/문자 출력하기/print-text.cpp
+++ 250228/문자 출력하기/print-text.cpp
@@ -6,8 +6,8 @@ int main() {
     string a;
     cin >> a;
 
-    for (i = 0; i < 8; i++) {
-        cout << a << endl;
+    for (int i = 0; i < 8; i++) {
+        cout << a;
     }
     return 0;
 }

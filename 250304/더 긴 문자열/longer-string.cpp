e75d0e1745017312e99cd12604048dbd7fec18a3
Shin Jia <134384192+shinjiaaa@users.noreply.github.com>
2025-03-04 16:38:25 +0900

    Title: 더 긴 문자열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250304/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+    if (a.length() == b.length()) {
+        cout << "same";
+    }
+    else if (a.length() > b.length()) {
+        cout << a << " " << a.length();
+    }
+    else {
+        cout << b << " " << b.length();
+    }
+    return 0;
+}

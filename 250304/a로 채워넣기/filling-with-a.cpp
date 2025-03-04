68d10ba79840a605943dc799436ea7eac3b4d21c
Shin Jia <134384192+shinjiaaa@users.noreply.github.com>
2025-03-04 16:44:19 +0900

    Title: a로 채워넣기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250304/a로 채워넣기/filling-with-a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+    int len = str.length();
+
+    str[1] = 'a';
+    str[len-2] = 'a';
+
+    cout << str;
+    return 0;
+}

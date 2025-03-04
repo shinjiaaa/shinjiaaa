250d899d6ac5a39de3ba9fe34c1e1ca7680d3392
Shin Jia <134384192+shinjiaaa@users.noreply.github.com>
2025-03-04 15:55:58 +0900

    Title: 배열 채우고 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250304/배열 채우고 출력하기/filling-array-and-print.cpp
+++ 250304/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -5,9 +5,9 @@ int main() {
     string arr[9];
 
     for (int i = 0; i < 10; i++) {
-        cin >> arr[i]
+        cin >> arr[i];
     }
 
-    cout >> arr[9] >> arr[8] >> arr[7] >> arr[6] >> arr[5] >> arr[4] >> arr[3] >> arr[2] >> arr[1] >> arr[0];
+    cout << arr[9] << arr[8] << arr[7] << arr[6] << arr[5] << arr[4] << arr[3] << arr[2] << arr[1] << arr[0];
     return 0;
 }

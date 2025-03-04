421621e552cb9430f413ead053498c1363d9db11
Shin Jia <134384192+shinjiaaa@users.noreply.github.com>
2025-03-04 15:54:32 +0900

    Title: 배열 채우고 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250304/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string arr[9];
+
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i]
+    }
+
+    cout >> arr[9] >> arr[8] >> arr[7] >> arr[6] >> arr[5] >> arr[4] >> arr[3] >> arr[2] >> arr[1] >> arr[0];
+    return 0;
+}

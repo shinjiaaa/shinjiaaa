59d0595308fc32af0d01d3596f8d30815f30e5c4
Shin Jia <134384192+shinjiaaa@users.noreply.github.com>
2025-03-05 13:39:43 +0900

    Title: 친근하지 않은 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250305/친근하지 않은 수/unfriendly-number.cpp
+++ 250305/친근하지 않은 수/unfriendly-number.cpp
@@ -7,7 +7,7 @@ int main() {
     int n = 0;
     cin >> N;
     
-    for (int i = 1; i >= N; i++) {
+    for (int i = 1; i <= N; i++) {
         if ((i % 2 == 0) || (i % 3 == 0) || (i % 5 == 0)) {
             continue;
         }
